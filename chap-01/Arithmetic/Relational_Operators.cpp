a1031fd97249fa881deac1cdf46d5a682ade4ecc
unknown <doniaskima344@gmail.com>
2023-02-10 15:00:16 +0100

    feat(Arithmetic):Relational_operators

---
+++ chap-01/Arithmetic/Relational_Operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    cout <<"Raltion Operators!"<<endl;
+
+    int a = 10;
+    int b = 5;
+
+    // True=1; False=0;
+    cout <<(a==b)<<endl;
+    cout <<(a!=b)<<endl;
+    cout <<(a<b)<<endl;
+    cout << (a>b)<<endl;
+    cout << (a<=b)<<endl;
+    cout << (a>=b)<<endl;
+    cout << !(a==b)<<endl; // (a!=b)
+}
