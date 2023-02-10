71ac88cf7ddf8d81189766b0dfc3191790b0cd6a
unknown <doniaskima344@gmail.com>
2023-02-10 14:46:07 +0100

    feat(Input):cin console input

---
+++ chap-01/Input/Input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    //cin - console input
+    int a;
+    cin >> a;
+    cout <<"Variables name a="<<a<<endl;
+
+    string name, username;
+    cout << "Enter your name :";
+    cin >> name;
+
+    cout <<"Enter your username :";
+    cin >> username;
+
+    cout <<"Welcome "<<name<<" "<<username<<endl;
+}
