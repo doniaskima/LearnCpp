51116ec388a2028e0166124a529d01fb0ae72a99
unknown <doniaskima344@gmail.com>
2023-02-10 14:51:08 +0100

    feat(Arithmetic):Arithmetic Assignment

---
+++ chap-01/Arithmetic/Arithmetic_Assignment_Increment_Decrement_Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+
+main(){
+    int a = 10, b = 5;
+
+    cout <<a+b<<endl; //Addition
+    cout << a - b << endl;//Substraction 
+    cout <<a/b<<endl; //Division
+    cout <<a*b<<endl; //Multiplication
+    cout <<a%b<<endl; //modulo->Remainder 
+    cout <<++a<<endl;//Pre-Increment
+    cout <<--a<<endl;//Pre-decrement
+    cout << a++ << endl; //Post Increament 
+    cout <<a--<<endl;//Post Decrement
+}
