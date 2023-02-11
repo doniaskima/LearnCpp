d74a8dd3da376aa46fece14b8d9f6c9b831fd1ab
unknown <doniaskima344@gmail.com>
2023-02-11 15:16:15 +0100

    feat(ReferenceVariables_Functions

---
+++ chap-01/Variables/ReferenceVariable_functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &, int &); 
+
+int main(){
+
+    int a = 10, b = 20;
+    swap(a, b);
+
+    cout<<"a : "<<a<<endl;
+    cout<<"b : "<<b<<endl;
+
+    return 0;
+}
+
+void swap(int &a,int &b){
+    int t;
+    t = a;
+    a = b;
+    b = t;
+}
