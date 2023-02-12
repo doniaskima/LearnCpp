78c04bfdb62939dbbd82ba55ced33e3b713b2e8e
unknown <doniaskima344@gmail.com>
2023-02-12 21:54:05 +0100

    variables

---
+++ chap-01/Variables/variable.py
@@ -0,0 +1,22 @@
+x = 10
+name = "John"
+
+x = 10
+
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+
+def add_numbers(x, y):
+    return x + y
+
+result = add_numbers(5, 3)
+print(result) # Output: 8
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1]) # Output: "banana"
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
