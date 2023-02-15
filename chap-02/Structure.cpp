c310ed3696cec63ec20bff2cbd51c78991ca0670
unknown <doniaskima344@gmail.com>
2023-02-15 22:48:02 +0100

    feat(structure)

---
+++ chap-02/Structure.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+struct personalData {
+    string name;
+    string address;
+    string contactNo;
+    short age;
+};
+
+void modify(personalData *);
+
+
+int main(){
+    personalData person[5];
+    person[0].name = "Donia Skima";
+    person[0].address = "Monastir";
+    person[0].contactNo = "+91 9000000000";
+    person[0].age = 21;
+    person[1].name = "Nikola Tesla";
+    cout<<person[0].name<<endl;
+    cout<<person[0].address<<endl;
+    cout<<person[0].contactNo<<endl;
+    cout<<person[0].age<<endl;
+
+    cout<<endl;
+
+    //Ways of fetching the values using pointer
+
+    cout << (*person).name << endl;
+    cout<<(*(person+1)).name<<endl;
+    cout<<(*&person[0]).name<<endl;
+    cout<<(person+1)->name<<endl;
+
+    personalData *p = person;
+    cout << p->name << endl;
+
+    cout << endl;
+
+    modify(p);
+    cout<<(p+1)->name<<endl;
+
+    return 0;
+
+}
+
+
+void modify(personalData *person){
+    (person + 1)->name = "Donia";
+}
