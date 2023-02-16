b180eca02a6b24729df423723e6c30272e77fb9c
unknown <doniaskima344@gmail.com>
2023-02-16 21:05:34 +0100

    Namesspaces

---
+++ chap-02/Namespaces.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+namespace A
+{
+    int var = 20;
+}
+
+namespace B
+{
+    int var = 40;
+}
+
+int main(){
+    int var = 50;
+    //Accessing var from namespace std
+    cout<<var<<endl;
+    //Accessing var from namespace A
+    cout<<A::var<<endl;
+    //Accessing var from namespace B
+    cout<<B::var<<endl;
+    return 0;
+}
