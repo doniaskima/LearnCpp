f2819d12c553f4adc5aa7c3d3e5f75d0521b6b68
unknown <doniaskima344@gmail.com>
2023-02-19 12:49:24 +0100

    add Readme

---
+++ S-Stack/ReverseStack.cpp
@@ -0,0 +1,7 @@
+// We have to reverse a stack using recursion.
+// First, we have called the reverse() function which will reverse our elements till stack is not empty.
+// Then we have called insert() function inside reverse() which would add all elements at bottom of new stack.
+// The insert() function will be the recursive function here.
+// This function take out the topmost element, pop it and recursively call itself to insert elements.
+// At last our original stack would get reversed.
+
