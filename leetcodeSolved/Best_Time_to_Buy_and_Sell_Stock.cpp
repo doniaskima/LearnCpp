0708718a28a95b8ab5246a8861c67377e91e5742
unknown <doniaskima344@gmail.com>
2023-07-07 19:15:45 +0100

    feat(EASSSSY Powerful set :tada: )

--- leetcodeSolved/Best_Time_to_Buy_and_Sell_Stock.cpp
+++ leetcodeSolved/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,39 +1,23 @@
 
-// %y solution but complexity baddd 😔
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int maximum = 0;
-        for (int i = 0; i < prices.size(); i++) {   
-            int initial = prices[i];
-            for (int j = i + 1; j < prices.size(); j++) {  
-                int output = prices[j] - initial;
-                if (output > maximum) {
-                    maximum = output;
-                }
-            }
-        }
-        return maximum;
-    }
-};
+// https://leetcode.com/problems/contains-duplicate/description/
 
-//Another solution 
+
+//Complexity O(1) Coool 😄 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int lsf = INT_MAX;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        set<int>s;
+        for(int i=0;i<n;i++){
+            s.insert(nums[i]);
         }
-        return op;
+        int m = s.size();
+        if(m==n){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
     }
 };
