0769eca84cd55afcea25f77e295262d52ee3df9a
unknown <doniaskima344@gmail.com>
2023-08-30 03:44:51 +0100

    featnot hard solved :pencil:

--- leetcodeSolved/search-in-rotated-sorted-array.cpp
+++ leetcodeSolved/search-in-rotated-sorted-array.cpp
@@ -2,3 +2,33 @@
 
 
 //an algorithm with O(log n) runtime complexity.
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1, mid;
+        while (l <= r) {  
+            mid = (l + r) / 2; 
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] >= nums[l]) {
+                if (target >= nums[l] && target <= nums[mid]) {
+                    r = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
+            else {
+                if (target >= nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                }
+                else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
