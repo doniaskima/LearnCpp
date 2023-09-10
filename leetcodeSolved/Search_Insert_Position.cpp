5c310af1e47052719c337dd46608af44d4fadb43
unknown <doniaskima344@gmail.com>
2023-09-11 00:37:27 +0100

    feat(next time inchallah)

---
+++ leetcodeSolved/Search_Insert_Position.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]<target) l= m+1;
+            else r=m-1;
+        }
+        return l;
+    }
+};
